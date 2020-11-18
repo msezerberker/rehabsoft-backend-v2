20ef81530d84a5e112f42fd98cb1d7e7782f174e
Oktay UGURLU <56557510+oktayugurlu@users.noreply.github.com>
2020-11-19 02:43:43 +0300

    PhysiotherapyPast and PhysiotherapyCentral entities and repos are added

--- src/main/java/com/hacettepe/rehabsoft/dto/GeneralEvaluationFormDto.java
+++ src/main/java/com/hacettepe/rehabsoft/dto/GeneralEvaluationFormDto.java
@@ -144,4 +144,5 @@ public class GeneralEvaluationFormDto {
     private Epilepsy epilepsy;
 
 
+
 }
---
+++ src/main/java/com/hacettepe/rehabsoft/repository/PhysiotherapyPastRepository.java
@@ -0,0 +1,7 @@
+package com.hacettepe.rehabsoft.repository;
+
+import com.hacettepe.rehabsoft.entity.PhysiotherapyPast;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhysiotherapyPastRepository extends JpaRepository<PhysiotherapyPast,Long> {
+}
